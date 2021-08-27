32dbc57053af9647d08dae5121d9443f664d362d
Bhagya Wijenayake <63334319+Bhagyawijenayake@users.noreply.github.com>
2021-08-27 21:45:51 +0530

    week6

--- keyboard.h
+++ keyboard.h
@@ -1,3 +1,4 @@
+
 #ifndef INCLUDE_KEYBOARD_H
 #define INCLUDE_KEYBOARD_H
 
@@ -7,4 +8,5 @@ unsigned char keyboard_read_scan_code(void);
 
 unsigned char keyboard_scan_code_to_ascii(unsigned char);
 
-#endif
+#endif /* INCLUDE_KEYBOARD_H */
+
